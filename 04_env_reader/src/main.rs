45c4422ab1e1a8830875d9f1ad568ec72c49c883
Ethan Harmon <ethanharmon2018@gmail.com>
2024-08-18 18:37:26 -0400

    Finished 04. Not sure why the second test is failing

--- 04_env_reader/src/main.rs
+++ 04_env_reader/src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate dotenv_codegen;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    todo!("replace this line with your code!")
+    dotenv().ok();
+    println!("{}", dotenv!("MY_VAR_1"));
+
+    match env::var_os("MY_VAR_2") {
+        Some(val) => println!("{val:?}"),
+        None => println!("default val"),
+    }
 }
--- 04_env_reader/tests/env_reader.rs
+++ 04_env_reader/tests/env_reader.rs
@@ -1,4 +1,37 @@
-fn reads_env_vars() -> Result<(), Box<dyn std::error::Error>> { 
-    todo!("put your integration test code here!");
+use assert_cmd::prelude::*;
+use core::panic;
+use std::process::Command;
+use std::{env, str};
+
+#[test]
+fn var_2_set() -> Result<(), Box<dyn std::error::Error>> {
+    let key = "MY_VAR_2";
+    unsafe {
+        env::set_var(key, "friends!");
+        let output_bytes = Command::cargo_bin("env_reader")?.output()?.stdout;
+
+        let output_str = match str::from_utf8(&output_bytes) {
+            Ok(val) => val,
+            Err(_) => panic!("Got non utf8 data from output_bytes"),
+        };
+
+        assert_eq!(output_str, "hello!!\n\"friends!\"\n");
+        env::remove_var(key);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn var_2_unset() -> Result<(), Box<dyn std::error::Error>> {
+    let output_bytes = Command::cargo_bin("env_reader")?.output()?.stdout;
+
+    let output_str = match str::from_utf8(&output_bytes) {
+        Ok(val) => val,
+        Err(_) => panic!("Got non utf8 data from output_bytes"),
+    };
+
+    assert_eq!(output_str, "hello!!\ndefault val\n");
+
     Ok(())
 }
