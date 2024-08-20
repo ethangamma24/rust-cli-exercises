e1559b4594439ddf45e7c9f3e1be130e16ebad53
Ethan Harmon <ethanharmon2018@gmail.com>
2024-08-19 21:10:36 -0400

    Created json file with data, successfully deserializing data in main(). Need to build table now

--- 05_table_of_chairs/src/main.rs
+++ 05_table_of_chairs/src/main.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
+use std::fs;
+
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
+pub struct Chair {
+    Name: String,
+    Price: String,
+    Color: String,
+    Quantity: u16,
+}
+
 fn main() {
-    todo!("replace this line with your code!")
+    let data: Vec<Chair> = {
+        let file_content = fs::read_to_string(
+            "/home/thetincan/Git/Learning/rust-cli-exercises/05_table_of_chairs/src/chairs.json",
+        )
+        .expect("Unable to read chairs.json");
+        serde_json::from_str(&file_content).expect("Error serializing json")
+    };
+
+    println!(
+        "{:?}",
+        serde_json::to_string_pretty(&data).expect("Error deserializing json")
+    );
 }
