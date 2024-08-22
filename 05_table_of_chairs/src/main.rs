aeee2bf7a50eb468f233afb49c25049390f3e4a7
Ethan Harmon <ethanharmon2018@gmail.com>
2024-08-21 20:40:13 -0400

    Got logic done for 05, need to write tests next

--- 05_table_of_chairs/src/main.rs
+++ 05_table_of_chairs/src/main.rs
@@ -1,3 +1,4 @@
+use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
 use std::fs;
@@ -20,13 +21,26 @@ fn main() {
         serde_json::from_str(&file_content).expect("Error serializing json")
     };
     let iterator = data.iter();
+    let mut table = vec![];
 
     for chair in iterator {
-        println!("{:?}", chair);
+        table.push(vec![
+            chair.Name.clone().cell(),
+            chair.Price.clone().cell(),
+            chair.Color.clone().cell(),
+            chair.Quantity.cell().justify(Justify::Right),
+        ]);
     }
 
-    println!(
-        "{:?}",
-        serde_json::to_string_pretty(&data).expect("Error deserializing json")
-    );
+    let table = table
+        .table()
+        .title(vec![
+            "Name".cell().bold(true),
+            "Price".cell().bold(true),
+            "Color".cell().bold(true),
+            "Quantity".cell().bold(true),
+        ])
+        .bold(true);
+
+    print_stdout(table);
 }
