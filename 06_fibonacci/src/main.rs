d8d2580f42b794e1671e58ae18ebcfcb695ef799
Ethan Harmon <ethanharmon2018@gmail.com>
2024-08-22 21:28:27 -0400

    Started work on fibonacci. Decided to be fancy and try to calculate whatever fib number is passed in (up to 256 at first), so I'll need to represent them as strings to get around 64 bit representation.

--- 06_fibonacci/src/main.rs
+++ 06_fibonacci/src/main.rs
@@ -1,3 +1,39 @@
+use core::panic;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    version,
+    about = "Calculates the nth number of the Fibonnaci Sequence. Largest number acceptable is 255"
+)]
+struct Args {
+    #[arg(required = true)]
+    seq_number: Option<u8>,
+}
+
 fn main() {
-    todo!("replace this line with your code!")
+    let args = Args::parse();
+
+    let seq_number = match args.seq_number {
+        Some(val) => val,
+        None => panic!("You must enter a number"),
+    };
+
+    let fib_number = calculate_fib(String::from("0"), String::from("0"), 0, seq_number);
+
+    println!("The number at position {seq_number:?} in the Fibonnaci Sequence is: {fib_number:?}");
+
+    ()
+}
+
+fn calculate_fib(prev: String, curr: String, counter: u8, max: u8) -> String {
+    if counter.eq(&max) {
+        curr
+    } else if prev.eq(&String::from("0")) && curr.eq(&String::from("0")) {
+        calculate_fib(String::from("0"), String::from("1"), 1, max)
+    } else {
+        let prev_chars: Vec<_> = prev.chars().collect();
+        let curr_chars: Vec<_> = curr.chars().collect();
+    }
 }
